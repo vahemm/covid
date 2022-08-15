d3b93682ffa37d002043caec49584a7a84acf161
Vahe Manukyan <vahemm@outlook.com>
2022-08-15 19:17:42 +0400

    Add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { VaccineTrackerModule } from './vaccine_tracker/vaccine_tracker.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/covid'), UsersModule, VaccineTrackerModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
